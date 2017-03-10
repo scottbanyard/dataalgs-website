390769434b1e4320bf67fa1a2302983673ec5db4
Scott Banyard <scottbanyard1@yahoo.co.uk>
2017-03-10 19:58:09 +0000

    Put in sqlite3, and got an example when database serializes

--- site/server.js
+++ site/server.js
@@ -6,9 +6,24 @@ var DEBUG = false;
 const http_1 = require("http");
 const fs = require("fs");
 const mime_types_1 = require("mime-types");
+const sqlite3 = require("sqlite3");
+sqlite3.verbose();
+var db = new sqlite3.Database(':memory:');
 var OK = 200, NotFound = 404, BadType = 415, Error = 500;
 var banned;
 start(8080);
+db.serialize(function () {
+    db.run("CREATE TABLE lorem (info TEXT)");
+    var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+    for (var i = 0; i < 10; i++) {
+        stmt.run("Ipsum " + i);
+    }
+    stmt.finalize();
+    db.each("SELECT rowid AS id, info FROM lorem", function (err, row) {
+        console.log(row.id + ": " + row.info);
+    });
+});
+db.close();
 function start(port) {
     banned = [];
     banUpperCase("./public/", "");

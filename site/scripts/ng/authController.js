6a6e36e2baacfd1eca9ef6eca205ee5bb3b5cb50
Scott Banyard <scottbanyard1@yahoo.co.uk>
2017-03-14 18:28:37 +0000

    Templete register and logins page on there, all setup on the controller ready to call login functions

--- site/scripts/ng/authController.js
+++ site/scripts/ng/authController.js
@@ -1,3 +1,12 @@
 angular.module('myApp').controller('authController', function($scope) {
-  // Grab text from input fields and check against database using login.js
+
+  $scope.registerPerson = function() {
+    console.log("Registering with: " + $scope.email + " , and password: " + $scope.password);
+    // Call register function
+  }
+
+  $scope.loginPerson = function() {
+    console.log("Logging in with: " + $scope.email + " , and password: " + $scope.password);
+    // Call login function
+  }
 });

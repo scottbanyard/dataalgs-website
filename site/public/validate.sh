15e28f999eed34a7b7e387062ecd1751ddc7f327
Scott Banyard <scottbanyard1@yahoo.co.uk>
2017-03-15 23:17:43 +0000

    Remove uneccessary public folder and scripts folder. All within dist now.

--- site/public/validate.sh
+++
@@ -1,8 +0,0 @@
-printf "*** Validation of HTML pages ***\n"
-counter=0
-for file in *.xhtml
-do
-  let counter="$counter"+1
-  printf "\n\n--- PAGE $counter : $file ---\n\n"
-  java -jar ../vnu.jar "$file"
-done
--- site/scripts/ng/homeController.js
+++
@@ -1,6 +0,0 @@
-angular.module('myApp').controller('homeController', function($scope) {
-
-  $scope.hello = function () {
-    console.log("Hello!");
-  }
-});

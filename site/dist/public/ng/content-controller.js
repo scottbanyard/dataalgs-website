f0bde3ca3f746b5e6a900fb6151ae9be649ff9d1
Scott Banyard <scottbanyard1@yahoo.co.uk>
2017-03-30 12:11:55 +0100

    Access page using page id where url is now content/pageid. If the user is authorised to view the page, then will get re-directed to home page

--- site/dist/public/ng/content-controller.js
+++ site/dist/public/ng/content-controller.js
@@ -1,8 +1,8 @@
 angular.module('myApp')
 .controller('contentController',
-    ($scope, contentService) => {
+    ($scope, contentService, $state, $stateParams) => {
         function getComments(){
-            contentService.getComments({ pageID:1 })
+            contentService.getComments({ pageID: $state.params.id })
                           .then((res) => {
                               var response = angular.fromJson(res).data;
                               if (response.success){
@@ -20,23 +20,22 @@ angular.module('myApp')
                 contentService.addComment({ token:localStorage.getItem('token'),
                                             comment: $scope.newComment,
                                             time: new Date().getTime(),
-                                            pageID: 1 });
+                                            pageID: $state.params.id});
                 getComments();
             }
         }
 
         contentService.getPage({token:localStorage.getItem('token'),
-                                  pageID: 1}
+                                  pageID: $state.params.id}
                               ).then((res) => {
                                   var response = angular.fromJson(res).data;
                                   if (response.success){
-
                                       $scope.pageInfo = response;
-                                      var content =
-                                      console.log($scope.pageInfo.htmlContent);
+                                      var content = $scope.pageInfo.htmlContent;
                                   }
-                                  else
-                                      $scope.pageInfo = {};
+                                  else {
+                                      // Don't have the access rights to view page or it doesn't exist, take back to home page.
+                                      $state.go('homePage');
                                   }
                               );
     });

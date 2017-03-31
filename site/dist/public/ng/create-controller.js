6c94f4fb1885e1f04071c2dae6ac501e0032998c
Scott Banyard <scottbanyard1@yahoo.co.uk>
2017-03-31 11:32:33 +0100

    Added validation to ensure all input new page details are filled in

--- site/dist/public/ng/create-controller.js
+++ site/dist/public/ng/create-controller.js
@@ -45,47 +45,76 @@ angular.module('myApp').controller('createController', ($rootScope, $scope, cont
     loadMarkdown();
   }
 
-  $scope.savePage = function () {
-    if ($scope.pageInfo != undefined) {
-      contentService.savePage({ token: localStorage.getItem('token'),
-                                Title: $scope.page.Title,
-                                Content: $("#myEditor").val(),
-                                PrivateView: $scope.page.PrivateView,
-                                PrivateEdit: $scope.page.PrivateEdit,
-                                LastEdit: new Date().getTime(),
-                                pageID: $state.params.id
-                                }).then((res) => {
-                                  swal({
-                                    html: true,
-                                    title: "<b>Success!</b>",
-                                    text: "You have successfully updated your page.",
-                                    type: "success"
-                                    },
-                                    function(){
-                                      swal.close();
-                                  });
-      });
+  function getError() {
+    var error = "";
+    // Undefined when new page, blank if deleted from a loaded page
+    if ($scope.page.Title == undefined || $scope.page.Title == "") {
+      error = "Please fill in the title of the page!";
+    } else if ($scope.page.PrivateView == undefined || $scope.page.PrivateView == "") {
+      error = "Please fill in whether you would like to make the page viewable to the public or private.";
+    } else if ($scope.page.PrivateEdit == undefined || $scope.page.PrivateEdit == "") {
+      error = "Please fill in whether you would like to make the page editable to the public or private.";
+    } else if ($("#myEditor").val() == "") {
+      error = "Please add some content to your page!";
+    }
+    return error;
+  }
 
-    } else {
-      console.log("new page");
-      contentService.savePage({ token: localStorage.getItem('token'),
-                                Title: $scope.page.Title,
-                                Content: $("#myEditor").val(),
-                                PrivateView: $scope.page.PrivateView,
-                                PrivateEdit: $scope.page.PrivateEdit,
-                                LastEdit: new Date().getTime(),
-                                pageID: null
-                                }).then((res) => {
-                                  swal({
-                                    html: true,
-                                    title: "<b>Success!</b>",
-                                    text: "You have successfully created a new page.",
-                                    type: "success"
-                                    },
-                                    function(){
-                                      swal.close();
-                                  });
+  $scope.savePage = function () {
+    var error = getError();
+
+    // If error, produce an alert, else proceed to saving page
+    if (error != "") {
+      swal({
+        title: "Error!",
+        text: error,
+        type: "error"
+        },
+        function(){
+          swal.close();
       });
+    } else {
+      // If undefined then it's a new page, otherwise it is a loaded page that you are updating
+      if ($scope.pageInfo != undefined) {
+        contentService.savePage({ token: localStorage.getItem('token'),
+                                  Title: $scope.page.Title,
+                                  Content: $("#myEditor").val(),
+                                  PrivateView: $scope.page.PrivateView,
+                                  PrivateEdit: $scope.page.PrivateEdit,
+                                  LastEdit: new Date().getTime(),
+                                  pageID: $state.params.id
+                                  }).then((res) => {
+                                    swal({
+                                      html: true,
+                                      title: "<b>Success!</b>",
+                                      text: "You have successfully updated your page.",
+                                      type: "success"
+                                      },
+                                      function(){
+                                        swal.close();
+                                    });
+        });
+
+      } else {
+        contentService.savePage({ token: localStorage.getItem('token'),
+                                  Title: $scope.page.Title,
+                                  Content: $("#myEditor").val(),
+                                  PrivateView: $scope.page.PrivateView,
+                                  PrivateEdit: $scope.page.PrivateEdit,
+                                  LastEdit: new Date().getTime(),
+                                  pageID: null
+                                  }).then((res) => {
+                                    swal({
+                                      html: true,
+                                      title: "<b>Success!</b>",
+                                      text: "You have successfully created a new page.",
+                                      type: "success"
+                                      },
+                                      function(){
+                                        swal.close();
+                                    });
+        });
+      }
     }
   }
 });
